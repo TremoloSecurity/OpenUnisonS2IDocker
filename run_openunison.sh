92e283c7724b3edb4db1235a851b6aead28bc471
mlbiam <marc.boorshtein@tremolosecurity.com>
2017-11-12 20:39:16 -0500

    make sure were only writing to the tmp dir

--- run_openunison.sh
+++ run_openunison.sh
@@ -3,6 +3,9 @@
 export OPENUNISON_DIR=/usr/local/openunison
 export OPENUNISON_WORK=$OPENUNISON_DIR/work
 
-export CLASSPATH="$OPENUNISON_WORK/lib/*:$OPENUNISON_WORK/classes:$OPENUNISON_DIR/quartz"
+#OpenShift really doesn't like writing outside of tmp
+mkdir /tmp/quartz
+mkdir /tmp/amq
+export CLASSPATH="$OPENUNISON_WORK/lib/*:$OPENUNISON_WORK/classes:/tmp/quartz"
 echo $CLASSPATH
 java -classpath $CLASSPATH $JAVA_OPTS com.tremolosecurity.openunison.undertow.OpenUnisonOnUndertow /etc/openunison/openunison.yaml
