9d54cb88d2d832fd00fcf2fa82368cb710b2d041
mlbiam <marc.boorshtein@tremolosecurity.com>
2017-12-06 15:08:35 -0600

    made heck_alive take params

--- check_alive.py
+++ check_alive.py
@@ -2,15 +2,22 @@
 
 import urllib2
 import ssl
-
+import sys
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+url_to_test = "https://127.0.0.1:8443/check_alive";
+text_to_find = 'Anonymous'
 
-res = urllib2.urlopen("https://127.0.0.1:8443/check_alive",context=ctx).read()
+if len(sys.argv) > 1:
+    url_to_test = sys.argv[1];
+    text_to_find = sys.argv[2];
 
-if res.find('Anonymous') > 0:
+
+res = urllib2.urlopen(url_to_test,context=ctx).read()
+
+if res.find(text_to_find) > 0:
     print "ok"
     exit(0)
 else:
